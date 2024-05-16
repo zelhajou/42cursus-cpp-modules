ee8700da8d95abecac08f391ac3e3c30b663e63d
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-05-16 18:27:32 +0100

    update

--- module 00/ex00/megaphone.cpp
+++ module 00/ex00/megaphone.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 14:09:13 by zelhajou          #+#    #+#             */
+/*   Updated: 2024/05/15 15:57:19 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main()
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
 {
-	std::cout << "Hello, World!\n" << std::endl;
-	return 0;
+	int i = 1;
+	int j = 0;
+	
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }

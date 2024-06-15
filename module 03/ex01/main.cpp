7a9bf8bd1794a3e9e087f76f0fbd716880191dd7
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-06-15 16:30:18 +0100

    ex01

--- module 03/ex01/main.cpp
+++ module 03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 20:31:21 by zelhajou          #+#    #+#             */
-/*   Updated: 2024/06/15 15:25:37 by zelhajou         ###   ########.fr       */
+/*   Updated: 2024/06/15 16:28:30 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int main() {
 
 	ClapTrap claptrap("Clap-9000");
 
-    claptrap.attack("Bandit");
+    claptrap.attack("Moulinex");
     claptrap.takeDamage(3);
     claptrap.beRepaired(5);
-    claptrap.attack("Spiderant");
+    claptrap.attack("Moulinex Beta");
     claptrap.takeDamage(12);
-    claptrap.attack("Badass Psycho");
+    claptrap.attack("Moulinex 2.0");
 
     std::cout << std::endl;
 
@@ -33,9 +33,9 @@ int main() {
 
     scavtrap.takeDamage(30);
     scavtrap.beRepaired(10);
-    scavtrap.attack("Spiderant");
+    scavtrap.attack("Moulinx");
     scavtrap.takeDamage(50);
-    scavtrap.attack("Badass Psycho");
+    scavtrap.attack("Moulinex 2.0");
     scavtrap.guardGate();
 
 	std::cout << std::endl;

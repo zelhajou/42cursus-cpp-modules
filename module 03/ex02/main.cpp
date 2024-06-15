bbb7a38b1fe966fc0f11231a6c41675cd6e9765b
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-06-15 16:30:28 +0100

    ex02

--- module 03/ex02/main.cpp
+++ module 03/ex02/main.cpp
@@ -6,39 +6,47 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 20:31:21 by zelhajou          #+#    #+#             */
-/*   Updated: 2024/06/15 15:25:37 by zelhajou         ###   ########.fr       */
+/*   Updated: 2024/06/15 16:29:43 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main() {
+    ClapTrap claptrap("Clap-9000");
 
-	
-
-	ClapTrap claptrap("Clap-9000");
-
-    claptrap.attack("Bandit");
+    claptrap.attack("Moulinex");
     claptrap.takeDamage(3);
     claptrap.beRepaired(5);
-    claptrap.attack("Spiderant");
+    claptrap.attack("Moulinex Beta");
     claptrap.takeDamage(12);
-    claptrap.attack("Badass Psycho");
+    claptrap.attack("Moulinex 2.0");
 
     std::cout << std::endl;
 
-    
     ScavTrap scavtrap("Scav-9000");
 
+    scavtrap.attack("Moulinex");
     scavtrap.takeDamage(30);
     scavtrap.beRepaired(10);
-    scavtrap.attack("Spiderant");
+    scavtrap.attack("Moulinex");
     scavtrap.takeDamage(50);
-    scavtrap.attack("Badass Psycho");
+    scavtrap.attack("Moulinex 2.0");
     scavtrap.guardGate();
 
-	std::cout << std::endl;
-	
-	return 0;
+    std::cout << std::endl;
+
+    FragTrap fragtrap("Frag-9000");
+
+    fragtrap.attack("Moulinex");
+    fragtrap.takeDamage(20);
+    fragtrap.beRepaired(15);
+    fragtrap.attack("Moulinox Beta");
+    fragtrap.takeDamage(40);
+    fragtrap.attack("Moulinex 2.0");
+    fragtrap.highFivesGuys();
+
+    return 0;
 }
